f1fda9c311886677f69772134e1f8020313269e5
Lupey1103 <luperon.christian95@gmail.com>
2023-08-31 14:42:49 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = (message) => {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    displayMessage('No number! ⛔');
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number! 🎉');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high! ⬆️' : 'Too low! ⬇️');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('Game Over! 🤡');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
